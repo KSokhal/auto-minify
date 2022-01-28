3e8f8e22a06d7b591ddb3a2156d2d1379f092697
KSokhal <52101915+KSokhal@users.noreply.github.com>
2022-01-28 09:05:20 +0000

    Updated

--- index.js
+++ index.js
@@ -4,49 +4,34 @@ const { minify } = require("terser");
 const fs = require('fs');
 var CleanCSS = require('clean-css');
 
-const mainPath = "./";
-// const mainPath = '../myriad/myriad_app/static/'
-
-// list all files in the directory
-// fs.readdir(mainPath, (err, files) => {
-//     if (err) {
-//         throw err;
-//     }
-
-//     // files object contains all files names
-//     // log them on console
-//     files.forEach(file => {
-//         console.log(file);
-//     });
-// });
+// const mainPath = "./";
+const mainPath = '../myriad/myriad_app/static/'
 
 chokidar.watch(mainPath + 'src').on('change', (filepath) => {
-    //   console.log(filepath);
     try {
         const ext = path.extname(filepath);
         var initialCode = fs.readFileSync(filepath, "utf8");
         var destinationPath = filepath.replace('src', './').replace(ext, `.min${ext}`)
 
-        if (ext == '.js') {
-            var minifiedCode = minify(initialCode)
+        switch (ext) {
+            case '.js':
+                var minifiedCode = minify(initialCode)
 
-            minifiedCode.then((result) => {
-                fs.writeFileSync(destinationPath, result.code, "utf8");
-            });
+                minifiedCode.then((result) => {
+                    fs.writeFileSync(destinationPath, result.code, "utf8");
+                });
+                break;
+            case '.css':
+                var minifiedCode = new CleanCSS({ level: 2 }).minify(initialCode);
 
-            var currentDate = new Date();
-
-        } else if (ext == '.css') {
-            var minifiedCode = new CleanCSS({ level: 2 }).minify(initialCode);
-
-            fs.writeFileSync(destinationPath, minifiedCode.styles, "utf8");
-
-            var currentDate = new Date();
-
+                fs.writeFileSync(destinationPath, minifiedCode.styles, "utf8");
+                break;
+            default:
+                break;
         }
 
+        var currentDate = new Date();
         console.log(`${currentDate.toLocaleTimeString()} - Minified '${filepath.replace(mainPath, "")}'`)
-
     } catch (err) {
         console.log(`Failed to minify ${filepath}: ${err}`);
     };
